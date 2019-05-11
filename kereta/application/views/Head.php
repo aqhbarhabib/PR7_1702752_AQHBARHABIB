6cc09cad5230734aebcf02d679ea846bfb817333
aqhbarhabib <aqhbarhabib5@gmail.com>
2019-05-11 21:38:45 +0700

    Mengganti warna tampilan

--- kereta/application/views/Head.php
+++ kereta/application/views/Head.php
@@ -10,8 +10,8 @@
 <body>
 
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#">KERETA JOIN PRA7</a>
-  <button class="navbar-toggler" aria-expanded="false" aria-controls="navbarColor01" aria-label="Toggle navigation" type="button" data-target="#navbarColor01" data-toggle="collapse">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-</nav>
+    <a class="navbar-brand" href="#">Kereta Join PR7</a>
+    <button class="navbar-toggler" aria-expanded="false" aria-controls="navbarColor01" aria-label="Toggle navigation" type="button" data-target="#navbarColor01" data-toggle="collapse">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </nav>
